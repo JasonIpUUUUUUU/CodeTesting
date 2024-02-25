fc3afc0704a0411e156f2f8d5844935a8cede3c6
JasonIpUUUUUUU <144679535+JasonIpUUUUUUU@users.noreply.github.com>
2024-02-25 22:35:19 +0000

    stuff

---
+++ GoogleEarth/GoogleEarthSetup.py
@@ -0,0 +1,10 @@
+#The following code is run on google Colab
+import ee
+import geemap
+
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import norm, gamma, f, chi2
+import IPython.display as disp
+%matplotlib inline
