6e257dac150ed4c20daa0515f574046013763ea8
JasonIpUUUUUUU <144679535+JasonIpUUUUUUU@users.noreply.github.com>
2024-02-25 22:46:08 +0000

    Update CodeFromWebsite.py

---
+++ GoogleEarth/CodeFromWebsite.py
@@ -0,0 +1,67 @@
+# This contains the code I copied from the website, the first link it outputs is to see the image and the second one is to download a TIFF image
+
+# Import the MODIS land cover collection.
+lc = ee.ImageCollection('MODIS/006/MCD12Q1')
+
+# Import the MODIS land surface temperature collection.
+lst = ee.ImageCollection('MODIS/006/MOD11A1')
+
+# Import the USGS ground elevation image.
+elv = ee.Image('USGS/SRTMGL1_003')
+
+# Initial date of interest (inclusive).
+i_date = '2017-01-01'
+
+# Final date of interest (exclusive).
+f_date = '2020-01-01'
+
+# Selection of appropriate bands and dates for LST.
+lst = lst.select('LST_Day_1km', 'QC_Day').filterDate(i_date, f_date)
+
+# Define the urban location of interest as a point near Lyon, France.
+u_lon = 4.8148
+u_lat = 45.7758
+u_poi = ee.Geometry.Point(u_lon, u_lat)
+
+# Define the rural location of interest as a point away from the city.
+r_lon = 5.175964
+r_lat = 45.574064
+r_poi = ee.Geometry.Point(r_lon, r_lat)
+
+scale = 1000  # scale in meters
+
+# Print the elevation near Lyon, France.
+elv_urban_point = elv.sample(u_poi, scale).first().get('elevation').getInfo()
+print('Ground elevation at urban point:', elv_urban_point, 'm')
+
+# Calculate and print the mean value of the LST collection at the point.
+lst_urban_point = lst.mean().sample(u_poi, scale).first().get('LST_Day_1km').getInfo()
+print('Average daytime LST at urban point:', round(lst_urban_point*0.02 -273.15, 2), '°C')
+
+# Print the land cover type at the point.
+lc_urban_point = lc.first().sample(u_poi, scale).first().get('LC_Type1').getInfo()
+print('Land cover value at urban point is:', lc_urban_point)
+
+# Define a region of interest with a buffer zone of 1000 km around Lyon.
+roi = u_poi.buffer(1e6)
+
+# Reduce the LST collection by mean.
+lst_img = lst.mean()
+
+# Adjust for scale factor.
+lst_img = lst_img.select('LST_Day_1km').multiply(0.02)
+
+# Convert Kelvin to Celsius.
+lst_img = lst_img.select('LST_Day_1km').add(-273.15)
+
+from IPython.display import Image
+
+# Create a URL to the styled image for a region around France.
+url = lst_img.getThumbUrl({
+    'min': 10, 'max': 30, 'dimensions': 512, 'region': roi,
+    'palette': ['blue', 'yellow', 'orange', 'red']})
+print(url)
+
+# Display the thumbnail land surface temperature in France.
+print('\nPlease wait while the thumbnail loads, it may take a moment...')
+Image(url=url)
