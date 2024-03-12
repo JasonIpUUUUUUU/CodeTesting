0ae6f2919c27bb0037cc33144ea81b5c04fed8a9
JasonIpUUUUUUU <144679535+JasonIpUUUUUUU@users.noreply.github.com>
2024-03-12 22:50:36 +0000

    useful package for summarising

--- AI_Tutor/HuggingFacesPT.py
+++ AI_Tutor/HuggingFacesPT.py
@@ -6,7 +6,7 @@ import evaluate
 import numpy as np
 
 billsum = load_dataset("billsum", split="ca_test")
-billsum = billsum.train_test_split(test_size=0.001)
+billsum = billsum.train_test_split(test_size=0.0001)
 
 #to access billsum data, you can use billsum["train"][0]
 
