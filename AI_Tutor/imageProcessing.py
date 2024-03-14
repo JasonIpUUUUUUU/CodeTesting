b3284c1e23863384fc023bf50402177ae0c375e5
JasonIpUUUUUUU <144679535+JasonIpUUUUUUU@users.noreply.github.com>
2024-03-14 22:25:11 +0000

    image processing

---
+++ AI_Tutor/imageProcessing.py
@@ -0,0 +1,14 @@
+import os
+import cv2 as cv
+
+folder_path = "Images"
+output_folder = "path"
+
+if os.path.exists(folder_path):
+    "print image folder found"
+else:
+    "print folder directory incorrect"
+
+images = os.listdir(folder_path)
+
+image_files = [file for file in images]
