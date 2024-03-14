0cc5597d3d1d27d7d94cd5e443d6239da71d21da
JasonIpUUUUUUU <144679535+JasonIpUUUUUUU@users.noreply.github.com>
2024-03-14 22:32:32 +0000

    debugging image access

--- AI_Tutor/imageProcessing.py
+++ AI_Tutor/imageProcessing.py
@@ -4,11 +4,11 @@ import cv2 as cv
 folder_path = "Images"
 output_folder = "path"
 
-if os.path.exists(folder_path):
-    print("image folder found")
-else:
-    print("image folder not found")
-
 images = os.listdir(folder_path)
 
 image_files = [file for file in images]
+
+img = cv.imread(image_files[0])
+
+cv.imshow("original image", img)
+cv.waitKey(0)
