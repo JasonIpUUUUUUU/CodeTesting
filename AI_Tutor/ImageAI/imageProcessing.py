dff6b0ce1aee1aea444ea963e295cce47821d070
JasonIpUUUUUUU <144679535+JasonIpUUUUUUU@users.noreply.github.com>
2024-03-15 21:46:20 +0000

    binarization of images

--- AI_Tutor/ImageAI/imageProcessing.py
+++ AI_Tutor/ImageAI/imageProcessing.py
@@ -11,9 +11,14 @@ image_files = [os.path.join(folder_path, file) for file in images]
 
 img = cv2.imread(image_files[0])
 
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+processed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-result = pytesseract.image_to_string(gray_img)
+thresh, im_bw = cv2.threshold(processed_img, 127,255, cv2.THRESH_BINARY)
+
+cv2.imshow("processed image", processed_img)
+cv2.waitKey(0)
+
+result = pytesseract.image_to_string(processed_img)
 
 output_file_path = os.path.join("results.txt")
 with open(output_file_path, 'w') as f:
