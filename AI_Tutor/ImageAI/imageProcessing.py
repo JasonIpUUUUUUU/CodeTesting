25f6870bfafafb46d60a531283018911d6e5d4f6
JasonIpUUUUUUU <144679535+JasonIpUUUUUUU@users.noreply.github.com>
2024-03-15 22:18:39 +0000

    preprocessing details

--- AI_Tutor/ImageAI/imageProcessing.py
+++ AI_Tutor/ImageAI/imageProcessing.py
@@ -1,6 +1,57 @@
 import os
 import cv2
 import pytesseract
+import numpy as np
+
+def noise_removal(image):
+    kernel = np.ones((1,1), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    kernel = np.ones((1,1), np.uint8) #redefined in case for special cases
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    image = cv2.medianBlur(image, 1)
+    return (image)
+
+#https://becominghuman.ai/how-to-automatically-deskew-straighten-a-text-image-using-opencv-a0c30aed83df
+def getSkewAngle(cvImage) -> float:
+    # Prep image, copy, convert to gray scale, blur, and threshold
+    newImage = cvImage.copy()
+    gray = cv2.cvtColor(newImage, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (9, 9), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    # Apply dilate to merge text into meaningful lines/paragraphs.
+    # Use larger kernel on X axis to merge characters into single line, cancelling out any spaces.
+    # But use smaller kernel on Y axis to separate between different blocks of text
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 5))
+    dilate = cv2.dilate(thresh, kernel, iterations=2)
+
+    # Find all contours
+    contours, hierarchy = cv2.findContours(dilate, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)
+    for c in contours:
+        rect = cv2.boundingRect(c)
+        x,y,w,h = rect
+        cv2.rectangle(newImage,(x,y),(x+w,y+h),(0,255,0),2)
+
+    # Find largest contour and surround in min area box
+    largestContour = contours[0]
+    print (len(contours))
+    minAreaRect = cv2.minAreaRect(largestContour)
+    cv2.imwrite("temp/boxes.jpg", newImage)
+    # Determine the angle. Convert it to the value that was originally used to obtain skewed image
+    angle = minAreaRect[-1]
+    if angle < -45:
+        angle = 90 + angle
+    return -1.0 * angle
+# Rotate the image around its center
+def rotateImage(cvImage, angle: float):
+    newImage = cvImage.copy()
+    (h, w) = newImage.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    newImage = cv2.warpAffine(newImage, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return newImage
 
 folder_path = "Images"
 output_folder = "path"
@@ -13,7 +64,9 @@ img = cv2.imread(image_files[0])
 
 processed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-thresh, im_bw = cv2.threshold(processed_img, 127,255, cv2.THRESH_BINARY)
+thresh, processed_img = cv2.threshold(processed_img, 190, 255, cv2.THRESH_BINARY)
+
+processed_img = noise_removal(processed_img)
 
 cv2.imshow("processed image", processed_img)
 cv2.waitKey(0)
